dcfd28394be66eec53d9046bb15bd873176f09b1
Rai Akmal <muhammadrai0504@gmail.com>
2024-05-16 10:02:59 +0700

    materi 1

--- src/components/materi/MateriDeterminan.vue
+++ src/components/materi/MateriDeterminan.vue
@@ -9,15 +9,65 @@
         adalah matriks yang memiliki jumlah baris dan kolom yang sama.
       </p>
       <div class="mt-5">
-        <h2 class="text-xl font-medium text-black">1. Matriks Ordo 2 x 2</h2>
-        <img src="/src/assets/foto_rumus/matriks-ordo-2x2.png" alt="matriks-ordo-2x2" />
-        <img src="/src/assets/foto_rumus/determinan-ordo-2x2.png" alt="determinan-ordo-2x2" />
+        <h2 class="text-xl font-medium text-black mb-3">Matriks Ordo 2 x 2</h2>
+        <p>
+          Matriks ordo 2x2 adalah sebuah array persegi dengan dua baris dan dua kolom. Setiap elemen
+          dalam matriks tersebut dapat diidentifikasi dengan menggunakan dua indeks: satu untuk
+          baris dan satu untuk kolom. Sebagai contoh, sebuah matriks ordo 2x2 dapat ditulis sebagai
+          berikut:
+        </p>
+        <div class="flex justify-center items-center mt-3">
+          <img
+            src="/src/assets/foto_rumus/matriks-ordo-2x2.png"
+            alt="matriks-ordo-2x2"
+            style="width: 150px"
+          />
+        </div>
+        <div class="flex justify-center items-center">
+          <img
+            src="/src/assets/foto_rumus/determinan-ordo-2x2.png"
+            alt="determinan-ordo-2x2"
+            style="width: 300px"
+          />
+        </div>
       </div>
       <div class="mt-5">
-        <h2 class="text-xl font-medium text-black">2. Matriks Ordo 3 x 3</h2>
-        <img src="/src/assets/foto_rumus/matriks-ordo-3x3.png" alt="matriks-ordo-3x3" />
-        <img src="/src/assets/foto_rumus/determinan-ordo-3x3.png" alt="determinan-ordo-3x3" />
+        <h2 class="text-xl font-medium text-black mb-3">Matriks Ordo 3 x 3</h2>
+        <p>
+          Matriks ordo 3x3 adalah sebuah array persegi dengan tiga baris dan tiga kolom. Seperti
+          halnya dengan matriks 2x2, setiap elemen dalam matriks 3x3 juga diidentifikasi dengan dua
+          indeks: satu untuk baris dan satu untuk kolom. Sebagai contoh, sebuah matriks ordo 3x3
+          dapat ditulis sebagai berikut:
+        </p>
+        <div class="flex justify-center items-center">
+          <img
+            src="/src/assets/foto_rumus/matriks-ordo-3x3.png"
+            alt="matriks-ordo-3x3"
+            style="width: 200px"
+          />
+        </div>
+        <div class="flex justify-center items-center">
+          <img
+            src="/src/assets/foto_rumus/det-ordo-3x3.png"
+            alt="det-ordo-3x3"
+            style="width: 400px"
+          />
+        </div>
+        <div class="flex justify-center items-center">
+          <img
+            src="/src/assets/foto_rumus/determinan-ordo-3x3.png"
+            alt="determinan-ordo-3x3"
+            style="width: 1000px"
+          />
+        </div>
       </div>
+      <p class="mt-5">
+        Matriks ordo 2x2 dan 3x3 banyak digunakan dalam berbagai bidang matematika dan ilmu terapan,
+        termasuk aljabar linear, analisis vektor, dan grafika komputer. Mereka digunakan untuk
+        merepresentasikan transformasi geometris, sistem persamaan linear, dan banyak lagi. Dengan
+        pemahaman tentang matriks ordo 2x2 dan 3x3, Anda dapat mengatasi berbagai masalah matematika
+        dan rekayasa yang melibatkan konsep-konsep tersebut.
+      </p>
     </div>
   </div>
 </template>
--- src/components/materi/MateriVektor.vue
+++ src/components/materi/MateriVektor.vue
@@ -1,9 +1,9 @@
 <template lang="">
   <!-- Materi Vektor -->
   <div class="materi-vektor mx-auto mb-10 max-w-sm">
-    <div class="text-xl font-medium text-black text-center">Perkalian Vektor</div>
-    <div class="space-y-4">
-      <h2 class="text-xl font-medium text-black">1. Dot Product</h2>
+    <div class="text-2xl font-medium text-black text-center mb-3">Perkalian Vektor</div>
+    <div class="mb-5 space-y-4">
+      <h2 class="text-xl font-medium text-black">Dot Product</h2>
       <p>
         Perkalian dot atau titik disebut juga perkalian skalar (scalar product). Hal itu dikarenakan
         perkalian tersebut akan menghasilkan skalar meskipun kedua pengalinya merupakan vektor.
@@ -14,15 +14,23 @@
       </p>
       <p>
         Dalam bentuk komponen vektor, bila A = [a1, a1, a3] dan B = [b1, b2, b3], maka A&#x25CF;B =
-        (a1.b1) + (a2.b2) + (a3&#x25CF;b3)
+        (a1&#x25CF;b1) + (a2&#x25CF;b2) + (a3&#x25CF;b3)
       </p>
-      <img src="/src/assets/foto_rumus/matriks-ordo-2x2.png" alt="matriks-ordo-2x2" />
-      <img src="/src/assets/foto_rumus/determinan-ordo-2x2.png" alt="determinan-ordo-2x2" />
     </div>
-    <div>
-      <h2 class="text-xl font-medium text-black">2. Matriks Ordo 3 x 3</h2>
-      <img src="/src/assets/foto_rumus/matriks-ordo-3x3.png" alt="matriks-ordo-3x3" />
-      <img src="/src/assets/foto_rumus/determinan-ordo-3x3.png" alt="determinan-ordo-3x3" />
+    <div class="mb-5 space-y-4">
+      <h2 class="text-xl font-medium text-black">Cross Product</h2>
+      <p>
+        Perkalian silang (cross product) disebut juga sebagai perkalian vektor (vektor product),
+        karena perkalian ini akan menghasilkan vektor lain.
+      </p>
+      <p>Perkalian vektor antara A dan B dinyatakan dengan A x B</p>
+      <div class="flex justify-center items-center mt-3">
+        <img
+          src="/src/assets/foto_rumus/cross-product.png"
+          alt="cross-product"
+          style="width: 600px"
+        />
+      </div>
     </div>
   </div>
 </template>
