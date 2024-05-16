de7754f48b102b8e93c878a338aaaf7b8e28c63a
geran357 <geranuser@gmail.com>
2024-05-16 11:58:08 +0700

    membuat halaman tentang

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,141 +1,270 @@
 <template>
-  <!---->
-  <div class="scroll-smooth">
-    <figure class="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800 mb-8">
-      <img
-        class="w-24 h-24 md:w-48 md:h-auto md:rounded-none mx-auto bg-red-100 bg-center"
-        src="/src/assets/foto_about/WhatsApp Image 2024-05-09 at 21.01.21.jpeg"
-        alt=""
-        width="384"
-        height="512"
-      />
-      <div class="pt-6 md:p-8 text-center md:text-left space-y-4">
-        <blockquote>
-          <p class="text-lg font-medium text-white">
-            “tugas saya disini saya mengembangkan page tentang dan mengembangkannya dengan baik,<br />
-            harapan saya anak muda terus bisa berkembang dan selalu belajar dari kesalahannya”
-          </p>
-        </blockquote>
-        <figcaption class="font-medium">
-          <div class="text-white-500 dark:text-sky-400">FAJAR GERAN ARIFIN</div>
-          <div class="text-white">membuat page tentang</div>
-        </figcaption>
-        <button
-          class="bg-sky-500 hover:bg-sky-700 rounded-lg transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300"
-        >
-          <p class="text-stone-50 hover uppercase subpixel-antialiased w-60">Instagram Saya</p>
-        </button>
-      </div>
-    </figure>
-    <!---->
-    <figure class="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800 mb-8">
-      <img
-        class="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto bg-red-100"
-        src=""
-        alt=""
-        width="384"
-        height="512"
-      />
-      <div class="pt-6 md:p-8 text-center md:text-left space-y-4">
-        <blockquote>
-          <p class="text-lg font-medium text-white">
-            “tugas saya disini saya mengembangkan page tentang dan mengembangkannya dengan baik,<br />
-            harapan saya anak muda terus bisa berkembang dan selalu belajar dari kesalahannya”
-          </p>
-        </blockquote>
-        <figcaption class="font-medium">
-          <div class="text-white-500 dark:text-sky-400">FAJAR GERAN ARIFIN</div>
-          <div class="text-white">membuat page tentang</div>
-        </figcaption>
-        <button
-          class="bg-sky-500 hover:bg-sky-700 rounded-lg transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300"
-        >
-          <p class="text-stone-50 hover uppercase subpixel-antialiased w-60">Instagram Saya</p>
-        </button>
-      </div>
-    </figure>
-    <!---->
-    <figure class="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800 mb-8">
-      <img
-        class="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto bg-red-100"
-        src="https://i.pximg.net/img-master/img/2024/05/09/08/43/24/118566698_p0_master1200.jpg"
-        alt=""
-        width="384"
-        height="512"
-      />
-      <div class="pt-6 md:p-8 text-center md:text-left space-y-4">
-        <blockquote>
-          <p class="text-lg font-medium text-white">
-            “tugas saya disini saya mengembangkan page tentang dan mengembangkannya dengan baik,<br />
-            harapan saya anak muda terus bisa berkembang dan selalu belajar dari kesalahannya”
-          </p>
-        </blockquote>
-        <figcaption class="font-medium">
-          <div class="text-white-500 dark:text-sky-400">FAJAR GERAN ARIFIN</div>
-          <div class="text-white">membuat page tentang</div>
-        </figcaption>
-        <button
-          class="bg-sky-500 hover:bg-sky-700 rounded-lg transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300"
-        >
-          <p class="text-stone-50 hover uppercase subpixel-antialiased w-60">Instagram Saya</p>
-        </button>
-      </div>
-    </figure>
-    <!---->
-    <figure class="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800 mb-8">
-      <img
-        class="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto bg-red-100"
-        src="https://i.pximg.net/img-master/img/2024/05/09/08/43/24/118566698_p0_master1200.jpg"
-        alt=""
-        width="384"
-        height="512"
-      />
-      <div class="pt-6 md:p-8 text-center md:text-left space-y-4">
-        <blockquote>
-          <p class="text-lg font-medium text-white">
-            “tugas saya disini saya mengembangkan page tentang dan mengembangkannya dengan baik,<br />
-            harapan saya anak muda terus bisa berkembang dan selalu belajar dari kesalahannya”
-          </p>
-        </blockquote>
-        <figcaption class="font-medium">
-          <div class="text-white-500 dark:text-sky-400">FAJAR GERAN ARIFIN</div>
-          <div class="text-white">membuat page tentang</div>
-        </figcaption>
-        <button
-          class="bg-sky-500 hover:bg-sky-700 rounded-lg transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300"
-        >
-          <p class="text-stone-50 hover uppercase subpixel-antialiased w-60">Instagram Saya</p>
-        </button>
-      </div>
-    </figure>
-    <!---->
-    <figure class="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800 mb-8">
-      <img
-        class="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto bg-red-100"
-        src="https://i.pximg.net/img-master/img/2024/05/09/08/43/24/118566698_p0_master1200.jpg"
-        alt=""
-        width="384"
-        height="512"
-      />
-      <div class="pt-6 md:p-8 text-center md:text-left space-y-4">
-        <blockquote>
-          <p class="text-lg font-medium text-white">
-            “tugas saya disini saya mengembangkan page tentang dan mengembangkannya dengan baik,<br />
-            harapan saya anak muda terus bisa berkembang dan selalu belajar dari kesalahannya”
-          </p>
-        </blockquote>
-        <figcaption class="font-medium">
-          <div class="text-white-500 dark:text-sky-400">FAJAR GERAN ARIFIN</div>
-          <div class="text-white">membuat page tentang</div>
-        </figcaption>
-        <button
-          class="bg-sky-500 hover:bg-sky-700 rounded-lg transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300"
-        >
-          <p class="text-stone-50 hover uppercase subpixel-antialiased w-60">Instagram Saya</p>
-        </button>
-      </div>
-    </figure>
+  <div class="animation1"></div>
+  
+  <div class="opacity">
+  <div class="my-8 text-2xl flex items-center justify-center animate-bounce">
+    <img src="/src/assets/foto_about/bigtdownarrow_83688.png" alt="Gambar" class="w-8 h-8 mr-2">
+    Scroll ke bawah!
   </div>
+
+  <div class="transition-transform transition ease-in-out delay-150">
+    <!---->
+    <div class="scroll-smooth">
+      <div class="scale:-500">
+        <figure class="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800 mb-8" >
+          <img
+            class="w-24 h-24 md:w-48 md:h-auto  mx-auto bg-red-100 bg-center "
+            src="/src/assets/foto_about/Gambar WhatsApp 2024-05-16 pukul 11.22.59_9f683f08.jpg"
+            alt=""
+            width="384"
+            height="512"
+          />
+          <div class="pt-6 md:p-8 text-center md:text-left space-y-4">
+            <blockquote>
+              <p class="text-lg font-medium text-white">
+                “tugas saya disini saya mengembangkan page tentang dan mengembangkannya dengan baik,<br />
+                untuk anak muda terus lah berkembang dan selalu belajar dari kesalahan”
+              </p>
+            </blockquote>
+            <figcaption class="font-medium">
+              <div class="text-white-500 dark:text-sky-400">Fajar Geran Arifim</div>
+              <div class="text-white">membuat page tentang</div>
+            </figcaption>
+            <a href="https://www.instagram.com/qx.nkp_amv/"
+   class="bg-sky-500 hover:bg-sky-700 rounded-lg transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300 inline-block"
+   style="text-align: center;">
+  <p class="text-stone-50 hover uppercase subpixel-antialiased w-60 mx-auto">Instagram Saya</p>
+</a>
+          </div>
+        </figure>
+      </div>
+    <!---->
+    <figure class="md:flex bg-slate-100  p-8 md:p-0 dark:bg-slate-800 mb-8">
+      <img
+        class="w-24 h-24 md:w-48 md:h-auto  mx-auto bg-red-100"
+        src="/src/assets/foto_about/Gambar WhatsApp 2024-05-11 pukul 18.49.01_d4b86803.jpg"
+        alt=""
+        width="384"
+        height="512"
+      />
+      <div class="pt-6 md:p-8 text-center md:text-left space-y-4">
+        <blockquote>
+          <p class="text-lg font-medium text-white">
+            “tugas saya disini saya menyusun materi dan menyusun laporan,<br />
+            Teruslah maju, lari jika bisa, berjalan jika<br /> perlu, merangkak jika harus, tapi jangan pernah menyerah pada apapun yang kamu hadapi”
+          </p>
+        </blockquote>
+        <figcaption class="font-medium">
+          <div class="text-white-500 dark:text-sky-400">Talitha Ramadhani Nur Azizah</div>
+          <div class="text-white">content writer</div>
+        </figcaption>
+        <a href="https://www.instagram.com/talitharna_?igsh=eXd6YWV2eGY5OWgw"
+   class="bg-sky-500 hover:bg-sky-700 rounded-lg transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300 inline-block"
+   style="text-align: center;">
+  <p class="text-stone-50 hover uppercase subpixel-antialiased w-60 mx-auto">Instagram Saya</p>
+</a>
+
+
+
+      </div>
+      
+    </figure>
+    <!---->
+    <figure class="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800 mb-8">
+      <img
+        class="w-24 h-24 md:w-48 md:h-auto  mx-auto bg-red-100 bg-contain"
+        src="/src/assets/foto_about/Gambar WhatsApp 2024-05-12 pukul 09.58.15_334a9976.jpg"
+        alt=""
+        width="384"
+        height="512"
+      />
+      <div class="pt-6 md:p-8 text-center md:text-left space-y-4">
+        <blockquote>
+          <p class="text-lg font-medium text-white">
+            “tugas saya disini Saya sebagai pencari konsep sekaligus desainer web,<br />
+            Harapan saya agar website ini bisa menginspirasi pembuat website lainnya sekaligus mengajarkan<br /> kalkulus”
+          </p>
+        </blockquote>
+        <figcaption class="font-medium">
+          <div class="text-white-500 dark:text-sky-400">Angra Advian Maulana</div>
+          <div class="text-white">pencari konsep sekaligus desainer web</div>
+        </figcaption>
+        <a href="https://www.instagram.com/angraadvian?igsh=MW5jcXBqOTExMXcycg=="
+        class="bg-sky-500 hover:bg-sky-700 rounded-lg transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300 inline-block"
+        style="text-align: center;">
+       <p class="text-stone-50 hover uppercase subpixel-antialiased w-60 mx-auto">Instagram Saya</p>
+     </a>
+      </div>
+    </figure>
+    <!---->
+    <figure class="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800 mb-8">
+      <img
+        class="w-24 h-24 md:w-48 md:h-auto  mx-auto bg-red-100"
+        src="/src/assets/foto_about/bertotot.jpg"
+        alt=""
+        width="384"
+        height="512"
+      />
+      <div class="pt-6 md:p-8 text-center md:text-left space-y-4">
+        <blockquote>
+          <p class="text-lg font-medium text-white">
+            “tugas saya disini saya mengembangkan page tentang dan mengembangkannya dengan baik,<br />
+            harapan saya anak muda terus bisa berkembang dan selalu belajar dari kesalahannya”
+          </p>
+        </blockquote>
+        <figcaption class="font-medium">
+          <div class="text-white-500 dark:text-sky-400">Fadhil Gani</div>
+          <div class="text-white">membuat page tentang</div>
+        </figcaption>
+        <a href="https://www.instagram.com/talitharna_?igsh=eXd6YWV2eGY5OWgw"
+        class="bg-sky-500 hover:bg-sky-700 rounded-lg transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300 inline-block"
+        style="text-align: center;">
+       <p class="text-stone-50 hover uppercase subpixel-antialiased w-60 mx-auto">Instagram Saya</p>
+     </a>
+      </div>
+    </figure>
+    <!---->
+    <figure class="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800 mb-8">
+      <img
+        class="w-24 h-24 md:w-48 md:h-auto  mx-auto bg-red-100"
+        src="/src/assets/foto_about/rai.jpg"
+        alt=""
+        width="384"
+        height="512"
+      />
+      <div class="pt-6 md:p-8 text-center md:text-left space-y-4">
+        <blockquote>
+          <p class="text-lg font-medium text-white">
+            “tugas saya disini saya menambahkan materi yang saya cari dan Memasukannya kedalam kodingan,<br />
+            Semoga tahun ini penuh dengan pembelajaran, pertumbuhan, dan kesempatan baru bagi Anda <br />sebagai mahasiswa.”
+          </p>
+        </blockquote>
+        <figcaption class="font-medium">
+          <div class="text-white-500 dark:text-sky-400">Muhammad Rai Akmal</div>
+          <div class="text-white">Memasukan materi kedalam kodingan</div>
+        </figcaption>
+        <a href="https://www.instagram.com/mhmmdraiakmal/"
+   class="bg-sky-500 hover:bg-sky-700 rounded-lg transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300 inline-block"
+   style="text-align: center;">
+  <p class="text-stone-50 hover uppercase subpixel-antialiased w-60 mx-auto">Instagram Saya</p>
+</a>
+      </div>
+      
+    </figure>
+    
+  </div>
+  
+  <!--form-->
+  <div class="flex justify-center items-center my-8">
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Feedback Form</title>
+</head>
+<body>
+  <div class="container">
+    
+    <!-- Formulir Feedback -->
+    
+    <form class="bg-orange-500 rounded-lg p-4 w-80 mx-auto" action="https://script.google.com/macros/s/AKfycbznIKHcGqRwd-ZWxkUSkemwN4ObbYQ1zGP8N2KeUFuSngdhX1GXhUrMKo6VKhIClH0r/exec" method="post">
+      <div class="space-y-4">
+        <h2 class="text-lg font-semibold leading-6 text-white">Feedback</h2>
+        <p class="text-sm leading-5 text-gray-100">Pendapat Anda sangat berarti bagi kami!</p>
+        <div class="grid grid-cols-1 gap-y-4">
+          <label for="username" class="block text-sm font-medium leading-5 text-white">Username</label>
+          <input type="text" name="Username" id="username" autocomplete="username" class="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
+          
+          <label for="email" class="block text-sm font-medium leading-5 text-white">Email address</label>
+          <input id="email" name="Email address" type="email" autocomplete="email" class="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" required pattern="[^@\s]+@[^@\s]+\.[^@\s]+">
+          
+          <label for="feedback" class="block text-sm font-medium leading-5 text-white">Your Feedback</label>
+          <textarea id="feedback" name="Your Feedback" rows="3" class="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"></textarea>
+        </div>
+        <div class="flex justify-end">
+          <button type="button" class="px-3 py-1 text-sm font-medium leading-5 text-white bg-orange-600 rounded-md hover:bg-orange-700 focus:outline-none focus:bg-orange-700">Cancel</button>
+          <button type="submit" class="ml-2 px-3 py-1 text-sm font-medium leading-5 text-white bg-indigo-600 rounded-md hover:bg-indigo-500 focus:outline-none focus:bg-indigo-700">Submit</button>
+        </div>
+      </div>
+    </form>
+    
+    
+  </div>
+</body>
+</html>
+
+  </div>
+  
+  
+</div>
+
+</div>
+
 </template>
 
-<style></style>
+<style>
+.scale-500 figure:hover {
+  transform: scale(1.1);
+ } /* Adjust the scale factor as needed */
+ .element {
+  transition: transform 0.3s ease; /* Efek transisi untuk perubahan transformasi */
+}
+
+.my-8 {
+  text-align: center;
+  font-weight: bold;
+}
+
+.scroll-smooth img {
+  object-fit: cover;
+}
+
+body {
+  animation: fadeInAnimation ease 3s;
+  animation-iteration-count: 1;
+  animation-fill-mode: forwards;
+}
+
+h1 {
+  color: green;
+}
+
+@keyframes fadeInAnimation {
+  0% {
+      opacity: 0;
+  }
+
+  100% {
+      opacity: 1;
+  }
+}
+
+button[type="submit"]:hover {
+  animation: pulse 0.5s ease infinite alternate;
+}
+
+@keyframes pulse {
+  0% {
+      transform: scale(1);
+  }
+  100% {
+      transform: scale(1.1);
+  }
+}
+
+button[type="submit"]:hover,
+button[type="button"]:hover {
+    animation: pulse 0.5s ease infinite alternate;
+}
+
+@keyframes pulse {
+    0% {
+        transform: scale(1);
+    }
+    100% {
+        transform: scale(1.1);
+    }
+}
+
+</style>
